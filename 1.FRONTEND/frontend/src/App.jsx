05d4203085f99781951a1e21235418b7b8853a2b
Venkat Praveen Venkatakrishnan <venkatpraveenv09@gmail.com>
2026-09-16 10:20:52 +0530

    Addig package jsons

--- 1.FRONTEND/frontend/src/App.jsx
+++ 1.FRONTEND/frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import Login from "./pages/login";
+import TeamMember from "./pages/teamMember/TeamMember"; 
 
 function App() {
   return <Login />;
+  
 }
 
+function App() {
+  return <TeamMember />;
+}
 export default App;
